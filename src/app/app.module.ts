681b79d6b14fe946f32779639461b353e34bfadb
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-02-04 12:46:24 -0500

    Import fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AuthFormComponent } from './Components/auth-form/auth-form.component';
+import { AuthFormComponent } from './components/auth-form/auth-form.component';
 import { FormsModule } from '@angular/forms';
-import { AuthRememberComponent } from './Components/auth-remember/auth-remember.component';
-import { AuthMessageComponent } from './Components/auth-message/auth-message.component';
-import { CreditCardDirective } from './Directives/credit-card.directive';
-import { FileSizePipe } from './Pipes/filesize.pipe';
+import { AuthRememberComponent } from './components/auth-remember/auth-remember.component';
+import { AuthMessageComponent } from './components/auth-message/auth-message.component';
+import { CreditCardDirective } from './directives/credit-card.directive';
+import { FileSizePipe } from './pipes/filesize.pipe';
 import { StockInventoryModule } from './stock-inventory/stock-inventory.module';
 
 @NgModule({
