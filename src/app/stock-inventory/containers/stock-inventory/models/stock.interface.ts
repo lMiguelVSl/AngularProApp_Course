b1067d8585099330cac4aa27008cb6995cff97d0
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-02-07 06:31:17 -0500

    New method to add stock - class

---
+++ src/app/stock-inventory/containers/stock-inventory/models/stock.interface.ts
@@ -0,0 +1,9 @@
+export class Stock {
+    product_id: string;
+    quantity: number;
+
+    constructor(product_id: string, quantity: number) {
+        this.product_id = product_id;
+        this.quantity = quantity;
+    }
+}
--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { FormArray, FormControl, FormGroup } from "@angular/forms";
 import { Product } from "./models/product.interface";
+import { Stock } from './models/stock.interface';
 
 @Component({
     selector: 'app-stock-inventory',
@@ -23,12 +24,22 @@ export class StockInventoryComponent {
             product_id: new FormControl(''),
             quantity: new FormControl(10)
         }),
-        stock: new FormArray([])
+        stock: new FormArray([
+            this.createStock(new Stock('10', 1)),
+            this.createStock(new Stock('20', 2))
+        ])
     })
 
     onSubmit() {
 
         console.log('FORM', this.form.value);
         console.log('FORM VALID', this.form.invalid);
+    }
+
+    createStock(stock: Stock) {
+        return new FormGroup({
+            product_id: new FormControl(stock.product_id),
+            quantity: new FormControl(stock.quantity)
+        });
     }
 }
